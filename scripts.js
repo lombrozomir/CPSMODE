376b6937090b3b4c29f336f0e090f53e61c08060
lombrozomir <roman.ryzhkov31@gmail.com>
2023-11-07 02:52:13 +0300

    0.0.9

---
+++ scripts.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper('.swiper-container__mobile', {
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+
+    scrollbar: {
+        hide: true
+    },
+
+
+});
